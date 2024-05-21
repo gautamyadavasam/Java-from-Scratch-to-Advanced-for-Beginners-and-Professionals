918f25a4975edb8911ee2e74f8273674a550d8dd
GAUTAM YADAV <138715315+gautamyadavasam@users.noreply.github.com>
2024-05-21 22:06:14 +0530

    java files are uploaded

---
+++ FirstClass.java
@@ -0,0 +1,8 @@
+class FirstClass {
+    public static void main(String args[]) {
+        //output
+        System.out.print("hello world\n");
+        System.out.print(" I am gautam yadav\n");
+        System.out.print(" I am gautam yadav");
+    }
+}     
---
+++ if_else_elseif_switch_break.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class if_else_elseif_switch_break {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int age = sc.nextInt();
+         
+        if(age>18) {
+            System.out.println("Adult");
+            
+        }
+
+    }
+    
+}
